6f07976315c6fded77b6a591e6b89bba592be249
Mario Sepúlveda <sepulvedamario87@gmail.com>
2023-11-28 09:48:32 -0500

    router fixed

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,7 +1,7 @@
 import {
     createBrowserRouter,
 } from "react-router-dom";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import Type from "../pages/Type";
 import Adjust from "../pages/Adjust";
 import Meter from "../pages/Meter";
